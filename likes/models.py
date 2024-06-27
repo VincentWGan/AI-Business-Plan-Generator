ed159c66195ceb7e928b5d66d117620a22fd608f
WenqinGan <wxg39@psu.edu>
2024-06-27 15:47:06 -0400

    Initialized app

---
+++ likes/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+# Create your models here.
+class LikedItem(models.Model):
+    # What user likes what object
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    # Type (product, video, article)
+    # ID
+    content_type = models.ForeignKey(to=ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
