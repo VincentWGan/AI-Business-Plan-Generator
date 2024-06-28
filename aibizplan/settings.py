937ee6f6b8de5f12dcc10edf58dc8856ed39680a
WenqinGan <wxg39@psu.edu>
2024-06-28 13:12:35 -0400

    Set up MySQL database

--- aibizplan/settings.py
+++ aibizplan/settings.py
@@ -86,8 +86,11 @@ WSGI_APPLICATION = 'aibizplan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aibizplan',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'momzij-4gobzy'
     }
 }
 
